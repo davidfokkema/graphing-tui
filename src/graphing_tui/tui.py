101e296ea88ec6f1565ba9eee11f09e644a33bfb
David Fokkema <davidfokkema@icloud.com>
2025-02-28 21:13:28 +0100

    Graph the function if it validates

--- src/graphing_tui/tui.py
+++ src/graphing_tui/tui.py
@@ -1,11 +1,12 @@
 import asteval
 import libcst as cst
+import numpy as np
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.validation import ValidationResult, Validator
 from textual.widgets import Input, Label
-from textual_plot import PlotWidget
+from textual_plot import HiResMode, PlotWidget
 
 
 class GraphingApp(App[None]):
@@ -23,6 +24,18 @@ class GraphingApp(App[None]):
                     validate_on=["changed"],
                     validators=ExpressionValidator(),
                 )
+
+    @on(Input.Changed)
+    def parse_expression(self, event: Input.Changed) -> None:
+        if event.validation_result.is_valid:
+            plot = self.query_one(PlotWidget)
+            plot.clear()
+            x = np.linspace(plot._x_min, plot._x_max, 101)
+            aeval = asteval.Interpreter(usersyms={"x": x})
+            y = aeval(event.value)
+            if not isinstance(y, np.ndarray):
+                y = np.full_like(x, fill_value=y)
+            plot.plot(x, y, hires_mode=HiResMode.BRAILLE)
 
 
 class ExpressionValidator(Validator):
